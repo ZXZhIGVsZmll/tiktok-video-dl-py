0baaddc103b6f63d052227018a0fcf1db9b830dc
ZXZhIGVsZmll <joshuaapalit@outlook.com>
2022-07-05 00:53:07 +0800

    Add optional argument --output

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-
 # tiktok-video-dl-py
 
-Download videos from TikTok from the command line.
+A small Python script to download videos from TikTok from the command line.
 
 # Disclaimer
 
@@ -12,10 +11,17 @@ Do not download videos which you do not have permission.
 Use git
 
 ```bash
-  git clone https://github.com/ZXZhIGVsZmll/tiktok-video-dl-py.git
+git clone https://github.com/ZXZhIGVsZmll/tiktok-video-dl-py.git
 ```
-    
+
 or download the `tiktok-vdl-cli.py` file.
+
+if you don't want to type python3 everytime you run the script
+
+```bash
+chmod u+x tiktok-vdl-cli.py
+```
+
 ## Usage/Examples
 
 Downloading Big Buck Bunny video
@@ -25,18 +31,18 @@ cd tiktok-video-dl-py
 python3 tiktok-vdl-cli.py https://www.tiktok.com/@arujiisochiasu/video/6895595198034873601
 ```
 
-or if you don't want to type python3 everytime
+or 
 
 ``` bash
-chmod u+x tiktok-vdl-cli.py
+cd tiktok-video-dl-py
 ./tiktok-vdl-cli.py https://www.tiktok.com/@arujiisochiasu/video/6895595198034873601
 ```
 
 Note: Remove ampersand(s) "&" from link if you don't want Bash to run it in the background.
 
-To show Help/Usage add the `--help` flag.
+To show Help/Usage use the `--help` flag.
 ## Roadmap
-
+- Add --ouput parameter :heavy_check_mark:
 - Add GUI
 
 
--- tiktok-vdl-cli.py
+++ tiktok-vdl-cli.py
@@ -11,26 +11,38 @@ import requests
 
 def main():
     """The main function"""
+    ## argparse
     parser = argparse.ArgumentParser(description='Download video from Tiktok')
     parser.add_argument('url', type=str, help='Provide a Tiktok post URL')
+    parser.add_argument('-o', '--output', type=str, help='Specify location or filename of downloaded file')
     args = parser.parse_args()
 
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+    url = args.url
+    filename = args.output
 
     # Prepairing the soup
-    req = requests.get(args.url, headers=headers)
+    req = requests.get(url, headers=headers)
     soup = bs4.BeautifulSoup(req.text, 'lxml')
     document = soup.select('#SIGI_STATE')[0].get_text()
 
     patt_input = r'\/video\/(\d+)'
-    vid_id = re.search(patt_input, args.url).group(1)
+    vid_id = re.search(patt_input, url).group(1)
     dl_url = json.loads(document)['ItemModule'][vid_id]['video']['downloadAddr']
     r_vid_id = requests.get(dl_url)
 
     # Writing to output to file
-    with open(vid_id + '.mp4', 'wb') as video_file:
-        video_file.write(r_vid_id.content)
-        print(f'Saved as {vid_id}.mp4')
+    if filename == None:
+        with open(vid_id + '.mp4', 'wb') as video_file:
+            video_file.write(r_vid_id.content)
+            print(f'Saved as {vid_id}.mp4')
+    else:
+        patt = r'\.mp4'
+        if re.search(patt, filename) == None:
+            filename = filename + '.mp4'
+        with open(filename, 'wb') as video_file:
+            video_file.write(r_vid_id.content)
+            print(f'Saved as {filename}')
 
 if __name__ == '__main__':
     main()
